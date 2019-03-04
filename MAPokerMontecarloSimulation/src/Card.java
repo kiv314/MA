53cf9bba5c46971f646a464a0f548d055dcc8f3c
kiv314 <kevinkosch314@gmail.com>
2019-03-05 00:01:08 +0100

    Kartendeck erstellt, 52 Karten mit Index 0-51, 4 Farben(0-3), Nummern

---
+++ MAPokerMontecarloSimulation/src/Card.java
@@ -0,0 +1,18 @@
+public class Card {
+	int nummer;
+	int farbe;	//Variablen
+	
+	public Card(int nummer, int farbe) {		// Konstruktor
+		this.nummer = nummer;
+		this.farbe = farbe;
+	}
+	
+	public int getNummer(){
+		return this.nummer;
+	}
+	public int getfarbe() {
+		return this.farbe;
+	}
+	
+}
+
--- MAPokerMontecarloSimulation/src/hauptclass.java
+++ MAPokerMontecarloSimulation/src/hauptclass.java
@@ -3,6 +3,15 @@ public class hauptclass {
 
 	public static void main(String[] args) {
 		
+		Card[] Karte;
+		Karte = new Card[52];
+		for(int i = 0; i < 52; i++) {
+			Karte[i] = new Card((i+1)%13,i%4); 
+		}
+		
+		for(int i = 0; i<10; i++) {
+			System.out.println(Karte[i].getfarbe());
+		}
 	}
 
 }
