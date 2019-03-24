df9ac6119f2d8b14ed6ed9682e6ade77522ea1c1
kiv314 <kevinkosch314@gmail.com>
2019-03-24 18:08:16 +0100

    Funktion tellCard erstellt Neu Karten nummer von 1-13

--- MAPokerMontecarloSimulation/src/hauptclass.java
+++ MAPokerMontecarloSimulation/src/hauptclass.java
@@ -1,17 +1,67 @@
 
 public class hauptclass {
+		
 
 	public static void main(String[] args) {
+		
 		
 		Card[] Karte;
 		Karte = new Card[52];
 		for(int i = 0; i < 52; i++) {
-			Karte[i] = new Card((i+1)%13,i%4); 
+			Karte[i] = new Card((i%13)+1,i%4); 
+		}
+		// 52 Karten erstellt mit Nummern: 1=ass; 2=2, etc bis 13=K, und farben 0=Heart(H), 1=Dimond(D), 2=Spade(S), 3clubs(C)
+		/*for(int i = 0; i<20; i++) {
+			System.out.println(Karte[i].getfarbe());
+		}*/
+		
+		
+		
+		tellCard(Karte[42]);
+		
+	}
+
+	private static void tellCard(Card card) {
+		String farbeBuchstabe = null;
+		String nummerBuchstabe = null;
+		int nummer = card.getNummer();
+		int farbeZahl = card.getfarbe();
+		switch (farbeZahl) {
+		case 0:
+			farbeBuchstabe = "H";
+			break;
+		case 1:
+			farbeBuchstabe = "D";
+			break;
+		case 2:
+			farbeBuchstabe = "S";
+			break;
+		case 3:
+			farbeBuchstabe = "C";
+			break;
+		}
+		if(nummer > 10) {
+			switch(nummer) {
+			case 11:
+				nummerBuchstabe = "J";
+				break;
+			case 12:
+				nummerBuchstabe = "D";
+				break;
+			case 13:
+				nummerBuchstabe = "K";
+				break;	
+			}
+		}
+		else if (nummer == 1) {
+			nummerBuchstabe = "A";
+		}
+		else {
+			nummerBuchstabe = Integer.toString(nummer);
 		}
 		
-		for(int i = 0; i<10; i++) {
-			System.out.println(Karte[i].getfarbe());
-		}
+		System.out.println("Karte: " + farbeBuchstabe + nummerBuchstabe);
+		
 	}
 
 }
