b9a506539ee86e4fb4f31fb7d8ec403ade337891
kiv314 <kevinkosch314@gmail.com>
2019-04-06 14:34:09 +0200

    erste Spielversuche mit flop und check und weitere funktionen wie gibBlatt zeig Soieler Blatt und zuAusführen

--- MAPokerMontecarloSimulation/src/Player1.java
+++ MAPokerMontecarloSimulation/src/Player1.java
@@ -1,58 +1,64 @@
-
+import java.util.Scanner;
 public class Player1 {
 	String spielerName;
 	boolean inRunde = false;
+	boolean manuel;
 	float anzahlBlattpaare = 0;
 	int position;
 	Card Blatt1 = null;
 	Card Blatt2 = null;;
-	Card Flop1  = null;
-	Card Flop2  = null;
-	
-	
-	public Player1(String spielerName, boolean inRunde, Card Blatt1, Card Blatt2) {		// Konstruktor
+	Card Flop1 = null;
+	Card Flop2 = null;
+
+	public Player1(String spielerName, boolean manuel, boolean inRunde, Card Blatt1, Card Blatt2) { // Konstruktor
 		this.inRunde = inRunde;
+		this.manuel = manuel;
 		this.spielerName = spielerName;
 		this.Blatt1 = Blatt1;
 		this.Blatt2 = Blatt2;
 	}
+
 	public void setBlatt1(Card Blatt) {
 		this.Blatt1 = Blatt;
 	}
+
 	public void setBlatt2(Card Blatt) {
 		this.Blatt2 = Blatt;
 	}
+
 	public void setInRunde(boolean inrunde) {
 		this.inRunde = inRunde;
 	}
-	
+
 	public Card getBlatt1() {
 		return Blatt1;
 	}
+
 	public Card getBlatt2() {
 		return Blatt2;
 	}
+
 	public boolean getInRunde() {
 		return this.inRunde;
 	}
-	
-	
-	
-	
-	
-	
+
 	public float getAnzahlBlattPaare() {
 		return anzahlBlattpaare;
 	}
-	
-	
-	public void machZug() {
-		if ((Blatt1.nummer == 1|| Blatt1.nummer == 13) && (Blatt2.nummer == 1|| Blatt2.nummer == 13)) {
-			System.out.println(spielerName + ": Habe ein Paar!");
-		}
-		
-	}
-	
-	
-}
 
+	public String machZug() {
+		if (manuel) {
+			System.out.println("machen Sie einen Zug");
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
+			return input;
+		}
+		if ((Blatt1.nummer == 2 || Blatt1.nummer == 3) && (Blatt2.nummer == 2 || Blatt2.nummer == 3)) {
+			return "fold";
+		} else {
+			return "check";
+		}
+
+	}
+
+}
--- MAPokerMontecarloSimulation/src/hauptclass.java
+++ MAPokerMontecarloSimulation/src/hauptclass.java
@@ -1,64 +1,87 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class hauptclass {
-		
 
 	public static void main(String[] args) {
 		Card Flop1 = null;
 		Card Flop2 = null;
 		Card Flop3 = null;
 		int runden = 10;
-		
+
 		Card[] Karte;
 		Karte = new Card[52];
-		for(int i = 0; i < 52; i++) {
-			Karte[i] = new Card((i%13)+1,i%4); 
+		for (int i = 0; i < 52; i++) {
+			Karte[i] = new Card((i % 13) + 1, i % 4);
 		}
-		// 52 Karten erstellt mit Nummern: 1=ass; 2=2, etc bis 13=K, und farben 0=Heart(H), 1=Dimond(D), 2=Spade(S), 3clubs(C)
-		/*for(int i = 0; i<20; i++) {
-			System.out.println(Karte[i].getfarbe());
-		}*/
-		
-		Player1 Spieler1 = new Player1 ("Spieler1", false, null, null);
-		Player1 Spieler2 = new Player1 ("Spieler2", false ,null, null);
+		// 52 Karten erstellt mit Nummern: 1=ass; 2=2, etc bis 13=K, und farben
+		// 0=Heart(H), 1=Dimond(D), 2=Spade(S), 3clubs(C)
+		/*
+		 * for(int i = 0; i<20; i++) { System.out.println(Karte[i].getfarbe()); }
+		 */
+
+		Player1 Spieler1 = new Player1("Ich", true, false, null, null);
+		Player1 Spieler2 = new Player1("Spieler2", false, false, null, null);
 		arrayMix(Karte);
-		
-		
+
 		Flop1 = Karte[2];
-		
-		for(int i = 0; i < runden; i+=1) {
+
+		do {
+			setzeSpielerInRunde(Spieler1, Spieler2);
 			arrayMix(Karte);
 			gibSpielerBlatt(Spieler1, Karte[0], Karte[1]);
-			Spieler1.machZug();	
+			spielerBlatt(Spieler1);
+			gibSpielerBlatt(Spieler2, Karte[2], Karte[3]);
+			zugAusführen(Spieler1);
+			zugAusführen(Spieler2);
+
+		} while (Spieler1.inRunde && Spieler2.inRunde);
+		if(Spieler1.inRunde) {System.out.println(Spieler1.spielerName + " hat gewonnen!");}
+		if(Spieler2.inRunde) {System.out.println(Spieler2.spielerName + " hat gewonnen!");}
+		/*
+		 * float wahrscheinlichkeitFürPaar =
+		 * (Spieler1.getAnzahlBlattPaare()/runden)*100;
+		 * System.out.println(wahrscheinlichkeitFürPaar); tellCard(Spieler1.Blatt1);
+		 */
+
+	}
+	private static void setzeSpielerInRunde(Player1... Spieler) {
+		for (int i=0; i < Spieler.length; i++) {
+			Spieler[i].inRunde = true;
 		}
-		
-		float wahrscheinlichkeitFürPaar = (Spieler1.getAnzahlBlattPaare()/runden)*100;
-		System.out.println(wahrscheinlichkeitFürPaar);
-		//tellCard(Spieler1.Blatt1);
-		
-		
 	}
 
-	
-	private static void findeSieger(Player1 Spieler1, Player1 Spieler2) {
+	private static void spielerBlatt(Player1 Spieler) {
+		System.out.println(Spieler.spielerName + ": " + tellCard(Spieler.Blatt1) + ", " + tellCard(Spieler.Blatt2));
 	}
-	
-	
-	/*private static boolean isflush(Card... Karte) {
-		boolean isflush;
-		if(Karte[2] == null) {
-			isflush = false;
+
+	private static void zugAusführen(Player1 Spieler) {
+		if (Spieler.inRunde) {
+			switch (Spieler.machZug()) {
+			case "fold":
+				Spieler.inRunde = false;
+				System.out.println(Spieler.spielerName + ": fold!");
+				break;
+			case "check":
+				System.out.println(Spieler.spielerName + ": Check!");
+				break;
+			}
 		}
-		else if(Karte.length == 5) {
-			//if()
-		}
-		else {
-			isflush = false;
-		}
-		
-		return isflush;
-	}*/
-	
-	private static void tellCard(Card card) {
+	}
+
+	// private static void findeSieger(Player1 Spieler1, Player1 Spieler2) {
+	// }
+
+	/*
+	 * private static boolean isflush(Card... Karte) { boolean isflush; if(Karte[2]
+	 * == null) { isflush = false; } else if(Karte.length == 5) { //if() } else {
+	 * isflush = false; }
+	 * 
+	 * return isflush; }
+	 */
+	// private static void
+
+	private static String tellCard(Card card) {
 		String farbeBuchstabe = null;
 		String nummerBuchstabe = null;
 		int nummer = card.getNummer();
@@ -77,8 +100,8 @@ public class hauptclass {
 			farbeBuchstabe = "C";
 			break;
 		}
-		if(nummer > 10) {
-			switch(nummer) {
+		if (nummer > 10) {
+			switch (nummer) {
 			case 11:
 				nummerBuchstabe = "J";
 				break;
@@ -87,44 +110,40 @@ public class hauptclass {
 				break;
 			case 13:
 				nummerBuchstabe = "K";
-				break;	
+				break;
 			}
-		}
-		else if (nummer == 1) {
+		} else if (nummer == 1) {
 			nummerBuchstabe = "A";
-		}
-		else {
+		} else {
 			nummerBuchstabe = Integer.toString(nummer);
 		}
-		System.out.println("Karte: " + farbeBuchstabe + nummerBuchstabe);
-		}
-	
-	/*private static void spieleRunde() {
-		
-	}*/
-	
-	
+		return "Karte: " + farbeBuchstabe + nummerBuchstabe;
+	}
+
+	/*
+	 * private static void spieleRunde() {
+	 * 
+	 * }
+	 */
+
 	private static void gibSpielerBlatt(Player1 Spieler, Card Karte1, Card Karte2) {
 		Spieler.setBlatt1(Karte1);
 		Spieler.setBlatt2(Karte2);
 	}
 
-	//private static void gibFlop()
-	
-	private static Card[] arrayMix(Card[] karten) { 
-		Card tmp; 
-		int rand; 
-	    Random r = new Random(); 
-	    for (int i = 0; i < karten.length; i++) { 
-	    	rand = r.nextInt(karten.length); 
-	    	tmp = karten[i]; 
-            karten[i] = karten[rand]; 
-            karten[rand] = tmp; 
-	        } 
-	    return karten; 
-	    } 
+	// private static void gibFlop()
 
-	   
-	
-	
+	private static Card[] arrayMix(Card[] karten) {
+		Card tmp;
+		int rand;
+		Random r = new Random();
+		for (int i = 0; i < karten.length; i++) {
+			rand = r.nextInt(karten.length);
+			tmp = karten[i];
+			karten[i] = karten[rand];
+			karten[rand] = tmp;
+		}
+		return karten;
+	}
+
 }
